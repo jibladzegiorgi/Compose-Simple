193b5c748331c00392af62f21b5a9ad55ed5931f
jibladzegiorgi <jibladzegiorgi94@gmail.com>
2022-08-12 19:14:41 +0400

    main screen

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose.screen.OnboardingPreview
+import com.example.compose.screen.OnboardingScreen
 import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,23 +23,66 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                   MyApp()
                 }
             }
         }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
+
+@Composable
+fun MyApp() {
+    var showOnboarding by remember { mutableStateOf(true) }
+    if (showOnboarding) {
+        OnboardingScreen(onClick = {
+            showOnboarding = false
+        })
+    } else {
+        Greeting("Android")
+        Greeting("World")
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        var expanded by remember{ mutableStateOf(false) }
+        val extraPadding = if (expanded) 48.dp else 0.dp
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello $name!")
+                Text(text = "Hello Compose!")
+            }
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                Text(if (expanded)"Show less" else "Show more")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     ComposeTheme {
-        Greeting("Android")
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Greeting("Android")
+            Greeting("asdasd")
+        }
     }
 }
