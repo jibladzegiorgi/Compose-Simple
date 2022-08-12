122fbdeceb603867ddf0fbaf0ca937b5fadff6d5
jibladzegiorgi <jibladzegiorgi94@gmail.com>
2022-08-12 19:14:56 +0400

    add onboarding screen

---
+++ app/src/main/java/com/example/compose/screen/OnboardingScreen.kt
@@ -0,0 +1,39 @@
+package com.example.compose.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose.ui.theme.ComposeTheme
+
+@Composable
+fun OnboardingScreen(onClick: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(modifier = Modifier.padding(vertical = 24.dp), onClick = onClick) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeTheme {
+        OnboardingScreen(onClick = {})
+    }
+}
