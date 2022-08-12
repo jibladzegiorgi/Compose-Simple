519c8b115438febf63f6e2a85b132d0f935e1932
jibladzegiorgi <jibladzegiorgi94@gmail.com>
2022-08-12 21:06:05 +0400

    add LazyColumn and change ui

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,17 +3,30 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.textInputServiceFactory
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
-import com.example.compose.screen.OnboardingPreview
 import com.example.compose.screen.OnboardingScreen
 import com.example.compose.ui.theme.ComposeTheme
 
@@ -26,49 +39,77 @@ class MainActivity : ComponentActivity() {
                 Column(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                   MyApp()
+                    MyApp()
                 }
             }
         }
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
     }
 }
 
 @Composable
 fun MyApp() {
-    var showOnboarding by remember { mutableStateOf(true) }
+    var showOnboarding by rememberSaveable { mutableStateOf(true) }
     if (showOnboarding) {
         OnboardingScreen(onClick = {
             showOnboarding = false
         })
     } else {
-        Greeting("Android")
-        Greeting("World")
+        Greeting()
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        var expanded by remember{ mutableStateOf(false) }
-        val extraPadding = if (expanded) 48.dp else 0.dp
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
+fun Greeting(nameList: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 5.dp)) {
+        items(items = nameList) { name ->
+            Surface(
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
             ) {
-                Text(text = "Hello $name!")
-                Text(text = "Hello Compose!")
-            }
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                Text(if (expanded)"Show less" else "Show more")
+
+                var expanded by rememberSaveable { mutableStateOf(false) }
+                val extraPadding by animateDpAsState(
+                    targetValue = if (expanded) 78.dp else 0.dp,
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+
+                Row(modifier = Modifier.padding(12.dp)
+                    .animateContentSize(
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioMediumBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )) {
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                    ) {
+                        Text(text = "Hello, ")
+                        Text(
+                            text = name,
+                            style = MaterialTheme.typography.h4.copy(
+                                fontWeight = FontWeight.ExtraBold
+                            )
+                        )
+                        if (expanded) {
+                            Text(
+                                text = ("Composem ipsum color sit lazy, " +
+                                        "padding theme elit, sed do bouncy. ").repeat(4),
+                            )
+                        }
+                    }
+
+                    IconButton(onClick = { expanded = !expanded }) {
+                        Icon(
+                            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                            contentDescription = null
+                        )
+                    }
+                }
             }
         }
     }
@@ -81,8 +122,7 @@ fun DefaultPreview() {
         Column(
             modifier = Modifier.fillMaxSize()
         ) {
-            Greeting("Android")
-            Greeting("asdasd")
+            Greeting()
         }
     }
 }
